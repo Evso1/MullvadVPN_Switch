8c74e5a79fcf020657c0aa7b4b4086e5eb2266d5
Evso1 <evanscott199@gmail.com>
2025-06-03 19:34:35 -0500

    Update runMullvad.sh

--- runMullvad.sh
+++ runMullvad.sh
@@ -3,7 +3,7 @@
 # === Mullvad Server Switcher Helper ===
 
 SCRIPT_NAME="mullvadSwitch.py"
-LOG_DIR="/home/burland/scripts/MullAuto/Logs"
+LOG_DIR="/path/tolog/directory"
 PID_FILE="/tmp/mullvadSwitch.pid"
 
 start_script(){
